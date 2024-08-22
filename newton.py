a2e20bb83a394c0aeec7c742db91f342eeb5a094
Xiang Li <xiang_li8@berkeley.edu>
2024-08-22 11:09:44 -0700

    Add basic implementation of Newton's method.

---
+++ newton.py
@@ -0,0 +1,15 @@
+def first_deriv(x0, f):
+    e = 2.22044604925e-16
+    return (f(x0 + e) - f(x0)) / e
+
+def second_deriv(x0, f):
+    e = 2.22044604925e-16
+    return (f(x0 + 2*e) - 2*f(x0 + e) + f(x0)) / (e**2)
+
+def optimize(x0, f):
+    e = 2.22044604925e-16
+    x1 = x0 - first_deriv(x0, f) / second_deriv(x0, f)
+    while abs(x1 - x0) > e:
+        x0 = x1
+        x1 = x0 - first_deriv(x0, f) / second_deriv(x0, f)
+    return x1
